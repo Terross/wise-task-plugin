d91acd3e0682fc44db37f592de46c7970038741c
Terross <dima38091@gmail.com>
2023-08-21 23:48:03 +0300

    Some fixes, move out migrations

--- src/main/java/ru/leti/wise/task/plugin/logic/DeletePluginOperation.java
+++ src/main/java/ru/leti/wise/task/plugin/logic/DeletePluginOperation.java
@@ -1,16 +1,9 @@
 package ru.leti.wise.task.plugin.logic;
 
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import ru.leti.wise.task.plugin.error.BusinessException;
-import ru.leti.wise.task.plugin.error.ErrorCode;
 import ru.leti.wise.task.plugin.repository.PluginRepository;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.UUID;
 
 @Component
